7fcc56ae9a8d1c2c3fe0965f233f90f1a75b721c
festus <kiamfestoh@gmail.com>
2018-12-03 14:30:32 +0300

    add domain repository

--- domain/src/main/java/com/example/domain/repository/IAuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/IAuthRepository.kt
@@ -1,5 +1,20 @@
 package com.example.domain.repository
 
+import com.example.domain.domainmodel.User
+import com.example.domain.domainmodel.Result
+
+
 /**
  * Created by Festus Kiambi on 12/3/18.
  */
+interface IAuthRepository {
+
+    suspend fun getCurrentUser(): Result<Exception, User?>
+
+    suspend fun signOutCurrentUser(): Result<Exception, Unit>
+
+    suspend fun deleteCurrentUser(): Result<Exception, Boolean>
+
+    suspend fun createGoogleUser(idToken: String): Result<Exception, Boolean>
+
+}
