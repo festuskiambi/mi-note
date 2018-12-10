a841fb7b0706f7752935ed47564f7a4fcf9a0214
festus <kiamfestoh@gmail.com>
2018-12-10 22:15:10 +0300

    create note detail classes

---
+++ app/src/main/java/com/example/festus/mi_note/notedetail/INoteDetailContract.kt
@@ -0,0 +1,5 @@
+package com.example.festus.mi_note.notedetail
+
+/**
+ * Created by Festus Kiambi on 12/10/18.
+ */
---
+++ app/src/main/java/com/example/festus/mi_note/notedetail/NoteDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.festus.mi_note.notedetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_detail)
+    }
+}
