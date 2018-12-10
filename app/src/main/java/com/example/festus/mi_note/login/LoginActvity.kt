a02a3546bb7d38eb354bc7541ae73f6da6571056
festus <kiamfestoh@gmail.com>
2018-12-10 10:00:08 +0300

    add login layout

--- app/src/main/java/com/example/festus/mi_note/login/LoginActvity.kt
+++ app/src/main/java/com/example/festus/mi_note/login/LoginActvity.kt
@@ -1,13 +1,14 @@
 package com.example.festus.mi_note.login
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.festus.mi_note.R
 
-class LoginActvity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 }
+
