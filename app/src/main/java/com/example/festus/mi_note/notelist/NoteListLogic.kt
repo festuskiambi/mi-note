357b05db5ff5ffaaed22e2e5249214fce748d569
festus <kiamfestoh@gmail.com>
2019-01-07 16:20:02 +0300

    set up noteListLogic tests

--- app/src/main/java/com/example/festus/mi_note/notelist/NoteListLogic.kt
+++ app/src/main/java/com/example/festus/mi_note/notelist/NoteListLogic.kt
@@ -1,5 +1,39 @@
 package com.example.festus.mi_note.notelist
 
+import com.example.domain.DispatcherProvider
+import com.example.domain.NoteServiceLocator
+import com.example.domain.UserServiceLocator
+import com.example.domain.interactor.AnonymousNoteSource
+import com.example.domain.interactor.AuthSource
+import com.example.domain.interactor.PublicNoteSource
+import com.example.domain.interactor.RegisteredNoteSource
+import com.example.festus.mi_note.common.BaseLogic
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
+
 /**
  * Created by Festus Kiambi on 1/7/19.
  */
+
+class NoteListLogic(
+    dispatcher: DispatcherProvider,
+    val userServiceLocator: UserServiceLocator,
+    val locator: NoteServiceLocator,
+    val view: INoteListContract.View,
+    val adapter: NoteListAdapter,
+    val vModel :INoteListContract.ViewModel,
+    val navigator: INoteListContract.Navigator,
+    val anonymous: AnonymousNoteSource,
+    val registered: RegisteredNoteSource,
+    val public: PublicNoteSource,
+    val authSource: AuthSource
+    ): BaseLogic(dispatcher), INoteListContract.Logic, CoroutineScope{
+
+    override fun event(event: NoteListEvent<Int>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override val coroutineContext: CoroutineContext
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+}
--- app/src/test/java/com/example/festus/mi_note/NoteListLogicTest.kt
+++ app/src/test/java/com/example/festus/mi_note/NoteListLogicTest.kt
@@ -1,9 +1,92 @@
 package com.example.festus.mi_note
 
+import com.example.domain.DispatcherProvider
+import com.example.domain.NoteServiceLocator
+import com.example.domain.UserServiceLocator
+import com.example.domain.domainmodel.Note
+import com.example.domain.domainmodel.User
+import com.example.domain.interactor.AnonymousNoteSource
+import com.example.domain.interactor.AuthSource
+import com.example.domain.interactor.PublicNoteSource
+import com.example.domain.interactor.RegisteredNoteSource
+import com.example.festus.mi_note.notelist.INoteListContract
+import com.example.festus.mi_note.notelist.NoteListAdapter
+import com.example.festus.mi_note.notelist.NoteListLogic
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import org.junit.jupiter.api.BeforeEach
+
 /**
  * Created by Festus Kiambi on 12/21/18.
  */
 
-class NoteListLogicTest{
+class NoteListLogicTest {
+
+    private val dispatcher: DispatcherProvider = mockk()
+    private val noteLocator: NoteServiceLocator = mockk()
+    private val userLocater: UserServiceLocator = mockk()
+    private val vModel: INoteListContract.ViewModel = mockk(relaxed = true)
+    private val view: INoteListContract.View = mockk(relaxed = true)
+    private val navigator: INoteListContract.Navigator = mockk(relaxed = true)
+    private val adapter: NoteListAdapter = mockk()
+    private val anonymous: AnonymousNoteSource = mockk()
+    private val registered: RegisteredNoteSource = mockk()
+    private val public: PublicNoteSource = mockk()
+    private val auth: AuthSource = mockk()
+
+
+    fun getNote(
+        creationDate: String = "28/10/2018",
+        contents: String = "When I understand that this glass is already broken, every moment with it becomes precious.",
+        upVotes: Int = 0,
+        imageUrl: String = "",
+        creator: User? = User(
+            "8675309",
+            "Ajahn Chah",
+            ""
+        )
+
+    ) = Note(
+        creationDate,
+        contents,
+        upVotes,
+        imageUrl,
+        creator
+    )
+
+    fun getUser(
+        uid: String = "8675309",
+        name: String = "Ajahn Chah",
+        profilePicUrl: String = ""
+    ) = User(uid, name, profilePicUrl)
+
+   private val getNoteList = listOf(getNote(), getNote(), getNote())
+
+    private val logic = NoteListLogic(
+        dispatcher,
+        userLocater,
+        noteLocator,
+        view,
+        adapter,
+        vModel,
+        navigator,
+        anonymous,
+        registered,
+        public,
+        auth
+    )
+
+
+    @BeforeEach
+    fun setUp() {
+        clearMocks()
+        every { dispatcher.provideUIContext() } returns Dispatchers.Unconfined
+
+    }
 
 }
+
+
